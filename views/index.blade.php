2d2e2804ac7a5b3dbcefe388f9cb2b4a8a2d4585
bestmomo <colorstonic@free.fr>
2015-11-20 16:42:45 +0100

    Change frontend framework

--- views/index.blade.php
+++ views/index.blade.php
@@ -2,61 +2,55 @@
 
 @section('content')
 
-        <div id="collapse_item" class="panel-group">
+        <ul class="collapsible popout" data-collapsible="accordion">
 
-          <?php $index = 0 ?>
-
-          @foreach($items as $item)
-              <div class="panel panel-info">
-                <div class="panel-heading"> 
-                  <h3 class="panel-title">
-                    <a href="#item{{ ++$index }}" data-parent="#collapse_item" data-toggle="collapse">{{ $item->getName() . ' (' . $item->getDescription() . ')' }}</a> 
-                  </h3>
-                </div>
-                <div id="item{{ $index }}" class="panel-collapse collapse {!! old('command') == $item->getName() ? 'in' : '' !!}">
-                  <div class="panel-body"> 
-                    <form method="POST" action="{!! url('niceartisan/item/' . $item->getName()) !!}">
-                      {!! csrf_field() !!}
-                      <input type="hidden" name="command" value="{{ $item->getName() }}">
-                      @if(count($item->getDefinition()->getArguments()) > 0)
-                        <fieldset>
-                            <legend>Arguments</legend>
-                            <div class="form-group">
-                                @foreach($item->getDefinition()->getArguments() as $argument)
-                                    <label class="control-label">{{ $argument->getName() }}</label>
-                                    <input type="text" class="form-control" name="argument_{{ $argument->getName() }}" placeholder="{{ $argument->getDefault() }}">
-                                    <span class="help-block">{{ $argument->getDescription() }}</span>
-                                @endforeach
-                            </div>
-                        </fieldset>
-                      @endif
-                      @if(count($item->getDefinition()->getOptions()) > 0)
-                        <fieldset>
-                            <legend>Options</legend>
-                            <div class="form-group">
-                              @foreach($item->getDefinition()->getOptions() as $option)
-                                @if($option->getDefault() !== false)
-                                  <label class="control-label">--{{ $option->getName() }}</label>
-                                  <input type="text" class="form-control" name="option_{{ $option->getName() }}" placeholder="{{ is_array($option->getDefault()) ? '' : $option->getDefault() }}">
-                                  <span class="help-block">{{ $option->getDescription() }}</span>
-                                @else
-                                  <div class="checkbox">
-                                    <label>
-                                      <input type="checkbox" name="option_{{ $option->getName() }}"> {{ $option->getDescription() }}
-                                    </label>
-                                  </div>
+            @foreach($items as $item)
+                <li>
+                    <div class="collapsible-header">{{ $item->getName() . ' (' . $item->getDescription() . ')' }}</div>
+                    <div class="collapsible-body">
+                        <div class="row">
+                            <form  class="col s12" method="POST" action="{!! url('niceartisan/item/' . $item->getName()) !!}">
+                                {!! csrf_field() !!}
+                                <input type="hidden" name="command" value="{{ $item->getName() }}">
+                                @if(count($item->getDefinition()->getArguments()) > 0)
+                                  <fieldset>
+                                      <legend>Arguments</legend>
+                                      @foreach($item->getDefinition()->getArguments() as $argument)
+                                            <div class="input-field">
+                                                <input type="text" name="argument_{{ $argument->getName() }}" placeholder="{{ $argument->getDefault() }}">
+                                                <label>{{ $argument->getName() . ' (' . $argument->getDescription() }})</label>
+                                            </div>
+                                      @endforeach
+                                  </fieldset>
                                 @endif
-                              @endforeach
-                            </div>
-                        </fieldset>
-                      @endif
-                      <button type="submit" class="btn btn-info">Go !</button>
-                    </form>                
-                  </div>
-                </div>
-              </div>            
-          @endforeach
+                                @if(count($item->getDefinition()->getOptions()) > 0)
+                                    <fieldset>
+                                        <legend>Options</legend>
+                                        @foreach($item->getDefinition()->getOptions() as $option)
+                                            @if($option->getDefault() !== false)
+                                                <div class="input-field">
+                                                    <input type="text" name="option_{{ $option->getName() }}" placeholder="{{ is_array($option->getDefault()) ? '' : $option->getDefault() }}">
+                                                    <label>--{{ $option->getName() . ' (' . $option->getDescription() }})</span></label>
+                                                </div>
+                                            @else
+                                                <p>
+                                                    <input type="checkbox" id="{{ $option->getDescription() }}" name="option_{{ $option->getName() }}">
+                                                    <label for="{{ $option->getDescription() }}">{{ $option->getDescription() }}</label>
+                                                </p>
+                                            @endif
+                                        @endforeach
+                                    </fieldset>
+                                @endif
+                                <br>
+                                <div class="col s12 center-align">
+                                    <button class="btn waves-effect waves-light" type="submit" name="action">Go !</button>
+                                </div>
+                            </form> 
+                        </div>
+                    </div>
+                </li>
+            @endforeach
 
-        </div>
+        </ul>
 
 @endsection
--- views/template.blade.php
+++ views/template.blade.php
@@ -5,68 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Nice Artisan</title>
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-        <style>
-            .navbar-default {
-                background-color: #598fb6;
-                border-color: #2c777d;
-            }
-            .navbar-default .navbar-brand {
-                color: #ebebe1;
-            }
-            .navbar-default .navbar-brand:hover, .navbar-default .navbar-brand:focus {
-                color: #fcfbfe;
-            }
-            .navbar-default .navbar-text {
-                color: #ebebe1;
-            }
-            .navbar-default .navbar-nav > li > a {
-                color: #ebebe1;
-            }
-            .navbar-default .navbar-nav > li > a:hover, .navbar-default .navbar-nav > li > a:focus {
-                color: #fcfbfe;
-            }
-            .navbar-default .navbar-nav > .active > a, .navbar-default .navbar-nav > .active > a:hover, .navbar-default .navbar-nav > .active > a:focus {
-                color: #fcfbfe;
-                background-color: #2c777d;
-            }
-            .navbar-default .navbar-nav > .open > a, .navbar-default .navbar-nav > .open > a:hover, .navbar-default .navbar-nav > .open > a:focus {
-                color: #fcfbfe;
-                background-color: #2c777d;
-            }
-            .navbar-default .navbar-toggle {
-                border-color: #2c777d;
-            }
-            .navbar-default .navbar-toggle:hover, .navbar-default .navbar-toggle:focus {
-                background-color: #2c777d;
-            }
-            .navbar-default .navbar-toggle .icon-bar {
-                background-color: #ebebe1;
-            }
-            .navbar-default .navbar-collapse,
-            .navbar-default .navbar-form {
-                border-color: #ebebe1;
-            }
-            .navbar-default .navbar-link {
-                color: #ebebe1;
-            }
-            .navbar-default .navbar-link:hover {
-                color: #fcfbfe;
-            }
-
-            @media (max-width: 767px) {
-                .navbar-default .navbar-nav .open .dropdown-menu > li > a {
-                    color: #ebebe1;
-                }
-                .navbar-default .navbar-nav .open .dropdown-menu > li > a:hover, .navbar-default .navbar-nav .open .dropdown-menu > li > a:focus {
-                    color: #fcfbfe;
-                }
-                .navbar-default .navbar-nav .open .dropdown-menu > .active > a, .navbar-default .navbar-nav .open .dropdown-menu > .active > a:hover, .navbar-default .navbar-nav .open .dropdown-menu > .active > a:focus {
-                    color: #fcfbfe;
-                    background-color: #2c777d;
-                }
-            }
-        </style>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css">
     </head>
 
     <body>
@@ -75,61 +14,69 @@
 
             <br>
 
-            <div class="container">
+            <h1 class="center-align">Nice Artisan</h1>
 
-                <h1 class="text-center">Nice Artisan</h1>
-
-                <nav class="navbar navbar-default">
-                    <div class="container-fluid">
-                        <ul class="nav navbar-nav">
-                            @for ($i = 0; $i < count($options); $i++)
-                                @if($i == 0)
-                                    <li {!! request()->is('niceartisan') || request()->is('niceartisan/' . $options[$i]) ? 'class="active"' : '' !!}>
-                                @else
-                                    <li {!! request()->is('niceartisan/' . $options[$i]) ? 'class="active"' : '' !!}>
-                                @endif
-                                        <a href="{!! url('niceartisan/' . $options[$i]) !!}">{{ ucfirst($options[$i]) }}</a>
-                                    </li>  
-                            @endfor
+            <nav>
+                <div class="nav-wrapper">
+                    <ul id="nav-mobile" class="left hide-on-med-and-down">
+                        @for ($i = 0; $i < count($options); $i++)
+                            @if($i == 0)
+                                <li {!! request()->is('niceartisan') || request()->is('niceartisan/' . $options[$i]) ? 'class="active"' : '' !!}>
+                            @else
+                                <li {!! request()->is('niceartisan/' . $options[$i]) ? 'class="active"' : '' !!}>
+                            @endif
+                                    <a href="{!! url('niceartisan/' . $options[$i]) !!}">{{ ucfirst($options[$i]) }}</a>
+                                </li>  
+                        @endfor
                     </ul>
                 </div>
             </nav>
+            
+            <div class="row">
 
-            @if (count($errors) > 0)
-                <div class="alert alert-danger">
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+                @if (count($errors) > 0)
+                    <div class="col s12">
+                      <div class="card red accent-4">
+                        <div class="card-content white-text">
+                            <span class="card-title">Whoops !<small> There were some problems with your input.</small></span>
+                            @foreach ($errors->all() as $error)
+                                <p>{{ $error }}</p>
+                            @endforeach
+                        </div>
+                      </div>
+                    </div>  
+                @endif
 
-            @if (session('output'))
-                <div class="alert alert-success">
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <strong>Success !</strong>
-                    <br><br>
-                    <pre>{!! session('output') !!}</pre>
-                </div>
-            @endif
+                @if (session('output'))
+                    <div class="col s12">
+                      <div class="card blue-grey darken-1">
+                        <div class="card-content white-text">
+                            <span class="card-title">Success !</span>
+                            <pre>{!! session('output') !!}</pre>
+                        </div>
+                      </div>
+                    </div>    
+                @endif
 
-            @if (session('error'))
-                <div class="alert alert-danger">
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-                    <strong>Error !</strong>
-                    <br><br>
-                    <pre>{!! session('error') !!}</pre>
-                </div>
-            @endif
+                @if (session('error'))
+                    <div class="col s12">
+                      <div class="card red accent-4">
+                        <div class="card-content white-text">
+                            <span class="card-title">Error !</span>
+                            <pre>{!! session('error') !!}</pre>
+                        </div>
+                      </div>
+                    </div>  
+                @endif
+                
+            </div>
 
             @yield('content')
 
         </div>
-
+        
         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js"></script>
+
     </body>
 </html>
